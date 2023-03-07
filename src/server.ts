db188210d9595c3aefd54a0a7d83fac2c0b170d9
montei_j <montei_j@etna-alternance.net>
2023-03-07 11:39:00 +0100

    [ADD] connection to mongodb database

--- src/server.ts
+++ src/server.ts
@@ -4,20 +4,27 @@ import cookieParser from 'cookie-parser';
 
 require('dotenv').config();
 
+// init app/middleware
 const port = process.env.SERVER_PORT;
 const app: Application = express();
 
+// cookie Parser setup
 app.use(cookieParser());
+
+// Cors setup
 app.use(cors({
   origin: `http://localhost:${process.env.CLIENT_PORT}`,
   credentials: true,
 }))
 
+// teeling express to use json
 app.use(express.json());
+
 
 // routes here
 
 
+
 app.listen(port, () => {
-  console.log(`Your API is now listening on port ${port}`);
-});  
+      console.log(`Your API is now listening on port ${port}`);
+    });
