d9128533e314aaa7c38049a7882bad9819fc8e2e
montei_j <montei_j@etna-alternance.net>
2023-03-23 13:45:55 +0100

    [UPDATE] deployment config for vercel

---
+++ dist/database/instance.js
@@ -0,0 +1,50 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.client = exports.db = void 0;
+const mongodb_1 = require("mongodb");
+require('dotenv').config();
+const user = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
+const uri = `mongodb+srv://${user}:${password}@cluster0.amzseaz.mongodb.net/?retryWrites=true&w=majority`;
+const client = new mongodb_1.MongoClient(uri);
+exports.client = client;
+function connectdb() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            yield client.connect();
+            yield client.db("admin").command({ ping: 1 });
+            console.log('Connected successfully to server');
+        }
+        catch (err) {
+            console.log('Connected to server refused/failed');
+        }
+        finally {
+            client.close();
+        }
+        // MongoClient.connect(`mongodb+srv://${user}:${password}@cluster0.amzseaz.mongodb.net/?retryWrites=true&w=majority`)
+        //   .then( client => {
+        //     dbConnection = client.db(process.env.DB_NAME);
+        //     console.log("connection ok");
+        //     return callback;
+        //   })
+        //   .catch( error => {
+        //     console.log(error);
+        //     return callback(error);
+        //   });
+    });
+}
+if (process.argv[2] === 'dbcheck') {
+    connectdb();
+}
+const db = client.db(process.env.DB_NAME);
+exports.db = db;
+//# sourceMappingURL=instance.js.map
